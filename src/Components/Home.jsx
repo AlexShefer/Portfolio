520bb4c00242b948bda46f3eee22098e0ea99349
Aleksandr Shefer <avshef451987@gmail.com>
2023-03-15 17:33:53 +0500

    Portfolio v01

---
+++ src/Components/Home.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
+
+function Home() {
+    return (
+        <div name="home" className=" w-full h-screen bg-[#0a192f]">
+            Home
+            {/* container */}
+            <div className="max-w-5xl mx-auto px-8 flex flex-col justify-center h-full">
+                <p className="text-pink-600">Hi my name is</p>
+                <h1 className="text-4-xl sm:text-7xl font-bold text-teal-100">
+                    Aleksandr Shefer
+                </h1>
+                <h2 className="text-4-xl sm:text-7xl font-bold text-teal-200">
+                    I am software engineer.
+                </h2>
+                <p className="text-teal-200 py-4 max-w-2xl">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Rem ad eius ipsa exercitationem rerum quam, accusantium
+                    excepturi, repudiandae ut laborum alias fugit molestiae quo,
+                    quisquam mollitia temporibus aperiam atque sint.
+                </p>
+                <div>
+                    <Link to="work" smooth={true} duration={500}>
+                        <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">
+                            View all work
+                            <span className="group-hover:rotate-90 duration-300">
+                                <HiArrowNarrowRight className="ml-3" />
+                            </span>
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Components/Skills.jsx
@@ -0,0 +1,94 @@
+import AWS from "../assets/aws.png";
+import CSS from "../assets/css.png";
+import Firebase from "../assets/firebase.png";
+import Github from "../assets/github.png";
+import HTML from "../assets/html.png";
+import JS from "../assets/javascript.png";
+import Mongo from "../assets/mongo.png";
+import Node from "../assets/node.png";
+import React from "../assets/react.png";
+import Tailwind from "../assets/tailwind.png";
+
+function Skills() {
+    return (
+        <div
+            name="skills"
+            className="w-full h-screen bg-[#0a192f] text-gray-300"
+        >
+            <div className="max-w-5xl mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div>
+                    <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+                        Skills
+                    </p>
+                    <p className="py-4">
+                        These are the technological I've worked with:
+                    </p>
+                </div>
+                <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={HTML}
+                            alt="Html icon"
+                        />
+                        <p className="my-4">HTML</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={CSS}
+                            alt="CSS icon"
+                        />
+                        <p className="my-4">CSS</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img className="w-20 mx-auto" src={JS} alt="JS icon" />
+                        <p className="my-4">Javascript</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={React}
+                            alt="React icon"
+                        />
+                        <p className="my-4">ReactJS</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={Node}
+                            alt="Node icon"
+                        />
+                        <p className="my-4">NodeJS</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={Mongo}
+                            alt="Mongo icon"
+                        />
+                        <p className="my-4">Mongo DB</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={Tailwind}
+                            alt="Tailwind icon"
+                        />
+                        <p className="my-4">Tailwind</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration:500">
+                        <img
+                            className="w-20 mx-auto"
+                            src={AWS}
+                            alt="AWS icon"
+                        />
+                        <p className="my-4">AWS</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
